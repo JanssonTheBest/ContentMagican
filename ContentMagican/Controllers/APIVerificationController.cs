9882cdc31206070764eff441b224a46086b0a25a
JanssonTheBest <yipandyaphack123@proton.me>
2024-11-29 20:34:54 +0100

    added tiktok api verification urlprops

---
+++ ContentMagican/Controllers/APIVerificationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContentMagican.Controllers
+{
+        [Route("tiktok_verify.txt")]
+        public class VerificationController : Controller
+        {
+            public IActionResult Index()
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "tiktokXsOLE8u4HYO2pcOTRIhcNtrlkkKW6ulr.txt");
+                var fileContent = System.IO.File.ReadAllText(filePath);
+                return Content(fileContent, "text/plain");
+            }
+        }
+}
