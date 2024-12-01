0806f1b2f25c4e83a0ade2d34f6a9bacfc191ea6
JanssonTheBest <yipandyaphack123@proton.me>
2024-12-01 06:39:03 +0100

    c

--- ContentMagican/Controllers/APIVerificationController.cs
+++ ContentMagican/Controllers/APIVerificationController.cs
@@ -7,7 +7,7 @@ namespace ContentMagican.Controllers
         {
             public IActionResult Index()
             {
-                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "tiktokXsOLE8u4HYO2pcOTRIhcNtrlkkKW6ulr.txt");
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "tiktok8UHXPdJlKE6pK7p699aVNgAZ2pW2hvP6.txt");
                 var fileContent = System.IO.File.ReadAllText(filePath);
                 return Content(fileContent, "text/plain");
             }
