de43754bd322435cdd0d9b9121c8fa5a55bbbeaa
JanssonTheBest <yipandyaphack123@proton.me>
2024-12-07 05:34:08 +0100

    fix

--- ContentMagican/Controllers/StripeWebhookController.cs
+++ ContentMagican/Controllers/StripeWebhookController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Stripe;
 using Stripe.Checkout;
+using System.Linq.Expressions;
 
 namespace ContentMagican.Controllers
 {
@@ -23,31 +24,51 @@ namespace ContentMagican.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> Payment()
+        public async Task<IActionResult> Handle()
         {
             var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
             var stripeSignature = Request.Headers["Stripe-Signature"];
 
             Event stripeEvent;
 
-            Session checkoutSession;
-
             try
             {
                 string secret = await _stripeRepository.GetCheckoutWebhookSecret();
                 stripeEvent = EventUtility.ConstructEvent(json, stripeSignature, secret);
-                checkoutSession = stripeEvent.Data.Object as Stripe.Checkout.Session;
             }
             catch (StripeException e)
             {
                 return BadRequest();
             }
 
+            switch (stripeEvent.Type)
+            {
+                case EventTypes.CheckoutSessionCompleted:
+                    return await HandleCheckout(stripeEvent);
+                case EventTypes.CustomerSubscriptionDeleted:
+
+                    break;
+            }
+
+            return BadRequest("Didnt match");
+        }
+
+
+
+        public async Task<IActionResult> HandleCheckout(Event stripeEvent)
+        {
+            Session checkoutSession = stripeEvent.Data.Object as Stripe.Checkout.Session;
+
+            if(checkoutSession.PaymentStatus != "paid")
+            {
+                return Ok("Not Paid");
+            }
+
             var result = _applicationDbContext.Users.Where(a => a.Id == Convert.ToInt32(checkoutSession.Metadata["UserId"])).FirstOrDefault();
 
             if (result == default)
             {
-                return Unauthorized("User not found");
+                return BadRequest("User not found");
             }
 
             try
@@ -61,6 +82,12 @@ namespace ContentMagican.Controllers
             }
 
             return Ok(result);
+        }
+
+
+        public async Task<IActionResult> HandleCustomerSubscriptionDeleted(Event stripeEvent)
+        {
+            return Ok();
         }
     }
 }
